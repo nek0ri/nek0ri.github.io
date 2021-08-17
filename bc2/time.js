f22bdce02e0a06f44572d454b29d445aa0df6f94
Matt L <mattlee999@gmail.com>
2021-08-17 11:24:26 +0800

    new

--- bc2/time.js
+++ bc2/time.js
@@ -1,4 +1,3 @@
-// Age calculation by @nekori
 var mmStamp = 31556736000;//一年走31556736戳
 var mmstT = Math.round(new Date()); //基准时间，单位为毫秒
 var mmsecPsT = 31; //每毫秒走了多少年，与10^12的乘积
@@ -9,10 +8,10 @@ var basicT = parseInt((stdT - year) * 1000000000000); //一年中过了多少，
 function time() {
 	var mmst = Math.round(new Date()) - mmstT; //距离刷新过了多少毫秒，即现在时间减去基准时间
 	var increase = mmst * mmsecPsT; //过去时间和单位时间的乘积
-	document.getElementById("time").style.color="black";
-	//document.getElementById("time").style.opacity=0.7;
+	document.getElementById("time").style.color="white";
+	document.getElementById("time").style.opacity=0.7;
 	document.getElementById("time").style.fontSize="large";
-	document.getElementById("time").style.fontFamily="Roboto Mono", "monospace";
-	document.getElementById("time").innerHTML = 'NOW ' + year + '.' + (basicT+increase);
+	document.getElementById("time").style.fontFamily="Orbitron", "sans-serif";
+	document.getElementById("time").innerHTML = year + '.' + (basicT+increase);
 };
 setInterval("time()", 10);
